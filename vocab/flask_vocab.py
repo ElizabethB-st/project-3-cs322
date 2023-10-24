c5196ef67cadc4e08c1d9ace25e6e1883ff875e9
ebowden05 <embowden05@gmail.com>
2023-10-23 18:16:07 -0700

    working draft

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -6,6 +6,8 @@ from a scrambled string)
 
 import flask
 import logging
+from flask import request
+
 
 # Our modules
 from src.letterbag import LetterBag
@@ -58,14 +60,14 @@ def index():
     return flask.render_template('vocab.html')
 
 
-@app.route("/keep_going")
-def keep_going():
-    """
-    After initial use of index, we keep the same scrambled
-    word and try to get more matches
-    """
-    flask.g.vocab = WORDS.as_list()
-    return flask.render_template('vocab.html')
+#@app.route("/keep_going")
+#def keep_going():
+   # """
+   # After initial use of index, we keep the same scrambled
+   # word and try to get more matches
+   # """
+   # flask.g.vocab = WORDS.as_list()
+   # return flask.render_template('vocab.html')
 
 
 @app.route("/success")
@@ -79,7 +81,7 @@ def success():
 #   a JSON request handler
 #######################
 
-@app.route("/_check", methods=["POST"])
+@app.route("/_check")
 def check():
     """
     User has submitted the form with a word ('attempt')
@@ -92,7 +94,7 @@ def check():
     app.logger.debug("Entering check")
 
     # The data we need, from form and from cookie
-    text = flask.request.form["attempt"]
+    text = request.args.get("text", type=str)
     jumble = flask.session["jumble"]
     matches = flask.session.get("matches", [])  # Default to empty list
 
@@ -105,22 +107,26 @@ def check():
         # Cool, they found a new word
         matches.append(text)
         flask.session["matches"] = matches
+        rslt = {"is_valid": True}
     elif text in matches:
         flask.flash("You already found {}".format(text))
+        rslt = {"is_valid": False}
     elif not matched:
         flask.flash("{} isn't in the list of words".format(text))
+        rslt = {"is_valid": False}
     elif not in_jumble:
         flask.flash(
             '"{}" can\'t be made from the letters {}'.format(text, jumble))
+        rslt = {"is_valid": False}
     else:
         app.logger.debug("This case shouldn't happen!")
         assert False  # Raises AssertionError
 
-    # Choose page:  Solved enough, or keep going?
+    # Solved enough, or keep going?
     if len(matches) >= flask.session["target_count"]:
-       return flask.redirect(flask.url_for("success"))
-    else:
-       return flask.redirect(flask.url_for("keep_going"))
+        rslt = {"is_valid": "done"}
+    return flask.jsonify(result=rslt)
+  
 
 
 ###############
